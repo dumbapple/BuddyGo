99a33a70b453565762cbd3d94e740d528f882e1b
Andrew Chang <ac.weichuan@gmail.com>
2020-09-12 16:32:47 -0700

    Set up server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const socket = require('socket.io');
+
+// App setup
+const app = express();
+const server = app.listen(8000, () => {
+    console.log('Listening for requests on port 8000!!!');
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup
+const io = socket(server);
+
+// socket is unique between particular client and the server
+io.on('connection', (socket) => { // runs callback on connection
+    console.log('Made socket connection!');
+});
